be4183c91fa8c803485dff4334293a599035612f
beefoo <bfoo@loc.gov>
2024-05-14 15:59:59 -0400

    Refactor data package scripts to use https:// links instead of AWS

--- Data Packages/README.md
+++ Data Packages/README.md
@@ -1,6 +1,6 @@
 # Resources for using Library of Congress Data Packages
 
-This is a growing collection of resources (Jupyter Notebooks, scripts, workflows) for accessing and using [data packages from the Library of Congress](https://labs.loc.gov/data/explore/).
+This is a growing collection of resources (Jupyter Notebooks, scripts, workflows) for accessing and using [data packages from the Library of Congress](https://data.labs.loc.gov/packages/).
 
 To run the Jupyter Notebooks you will need to change into this directory and install the required Python libraries:
 
--- Data Packages/helpers.py
+++ Data Packages/helpers.py
@@ -1,28 +1,24 @@
 import os
 
-def get_s3_bucket_stats(s3, bucket_name, prefix):
+def get_file_stats(files):
     
     # For keeping track of files, grouped by file type
     ftypes = {}
 
-    # Since list_objects_v2 is limited to 1000 results, we need to paginate the results
-    paginator = s3.get_paginator("list_objects_v2")
-    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
-    for page in page_iterator:
+    # Loop through files
+    for file in files:
 
-        # Loop through the results
-        for obj in page.get('Contents', []):
-            filename, file_extension = os.path.splitext(obj['Key'])
+        filename, file_extension = os.path.splitext(file['object_key'])
 
-            # Skip directories
-            if file_extension == '':
-                continue
-            
-            # Keep track of count and size
-            if file_extension not in ftypes:
-                ftypes[file_extension] = {'Count': 0, 'Size': 0}
-            ftypes[file_extension]['Count'] += 1
-            ftypes[file_extension]['Size'] += obj['Size']
+        # Skip directories
+        if file_extension == '':
+            continue
+        
+        # Keep track of count and size
+        if file_extension not in ftypes:
+            ftypes[file_extension] = {'Count': 0, 'Size': 0}
+        ftypes[file_extension]['Count'] += 1
+        ftypes[file_extension]['Size'] += file['size']
 
     # Convert to list, sort file types by total size
     stats = [{'FileType': ext, 'Count': ftypes[ext]['Count'], 'Size': ftypes[ext]['Size']} for ext in ftypes]
